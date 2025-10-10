0e7f2c23965768f5debf8a88f8f8b0af5f538c5c
kisun <kisun668@gmail.com>
2025-10-10 10:31:27 +0800

    注释补充

--- ps/info/disk_windows.go
+++ ps/info/disk_windows.go
@@ -22,7 +22,7 @@ func QueryDisks() (disks []Disk, err error) {
 			return nil, errors.Wrapf(err, "GetDiskGeometry for %s", diskPath)
 		}
 		// 注意：现代硬盘的真实容量不再与 CHS 信息匹配
-		// 因此，通过CHS计算出来的实际容量会比真实硬盘容量更小，请使用 IOCTL_DISK_GET_LENGTH_INFO 获取真实硬盘容量
+		// 因此，通过CHS计算出来的实际容量会比真实硬盘容量更小，实际场景中请使用 IOCTL_DISK_GET_LENGTH_INFO 获取真实硬盘容量
 		d.Sectors = int64(geo.Cylinders) * int64(geo.TracksPerCylinder) * int64(geo.SectorsPerTrack)
 		d.SectorSize = int(geo.BytesPerSector)
 		size, err := extend.FileSize(diskPath)
