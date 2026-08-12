76473152fd50aebdca2566e12ad230d35531fcbb
kisun <kisun668@gmail.com>
2026-08-12 10:06:57 +0800

    feat: 支持qcow2

--- examples/image/main.go
+++ examples/image/main.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dustin/go-humanize"
+	"github.com/kisun-bit/drpkg/disk/image/qcow2"
 	"github.com/kisun-bit/drpkg/disk/image/qemublk"
 	"github.com/kisun-bit/drpkg/logger"
 )
@@ -125,6 +126,88 @@ func WriteProf(enableHash bool) {
 	logger.Debugf(output)
 }
 
+func WriteProfv2(enableHash bool) {
+	if len(os.Args) < 3 {
+		logger.Fatal("Usage: <cmd> <qcow2_path>")
+	}
+
+	hash := md5.New()
+	logger.Debugf("target file: %s", os.Args[2])
+
+	factory := qcow2.NewImageFactory(true)
+	img, err := factory.OpenImage(os.Args[2], 512)
+	if err != nil {
+		logger.Fatal("Open: ", err)
+	}
+	defer img.Close()
+
+	// 获取镜像总大小，避免无限写入
+	totalSize := img.Size()
+	if totalSize <= 0 {
+		logger.Fatal("Invalid image size")
+	}
+
+	const bufLen = 2 << 20 // 2MB
+	buf := make([]byte, bufLen)
+	rand.Read(buf)
+
+	var off int64
+	done := make(chan struct{})
+
+	// 速度监控协程
+	go func() {
+		tik := time.NewTicker(5 * time.Second)
+		defer tik.Stop()
+		lastBytes := int64(0)
+
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-tik.C:
+				curBytes := off
+				duRBytes := curBytes - lastBytes
+				// 防止除零及精度问题
+				speed := uint64(float64(duRBytes) / 5.0)
+				lastBytes = curBytes
+
+				fmt.Printf("[%s] Written: %s, Speed: %s/s\n",
+					t.Format("15:04:05"),
+					humanize.IBytes(uint64(curBytes)),
+					humanize.IBytes(speed),
+				)
+			}
+		}
+	}()
+
+	// 主写入循环：按镜像大小精确控制
+	for uint64(off) < totalSize {
+		writeLen := uint64(bufLen)
+		if remaining := totalSize - uint64(off); remaining < writeLen {
+			writeLen = remaining
+		}
+
+		if ew := img.WriteAt(uint64(off), buf[:writeLen]); ew != nil {
+			logger.Error("WriteAt failed at offset ", off, ": ", ew)
+			break
+		}
+
+		if enableHash {
+			_, _ = hash.Write(buf[:writeLen])
+		}
+		off += int64(writeLen)
+	}
+
+	// ✅ 关键：通知监控协程退出
+	close(done)
+
+	output := fmt.Sprintf("Written: %d / %d bytes", off, totalSize)
+	if enableHash {
+		output += fmt.Sprintf(", MD5: %x", hash.Sum(nil))
+	}
+	logger.Info(output)
+}
+
 //func DemoRead(enableHash bool) {
 //	hash := md5.New()
 //
@@ -194,5 +277,6 @@ func main() {
 	//DemoWrite(false)
 	//DemoImageMap()
 
-	WriteProf(false)
+	//WriteProf(false)
+	WriteProfv2(false)
 }
--- extend/err.go
+++ extend/err.go
@@ -1,1 +1,49 @@
 package extend
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type ReRaisableError struct {
+	message      string
+	currentError error
+	base         error
+}
+
+func (err *ReRaisableError) Error() string {
+	current := error(err)
+	if reRaisable, ok := current.(*ReRaisableError); ok {
+		return reRaisable.base.Error() + "\n" + reRaisable.message
+	} else {
+		return current.Error()
+	}
+}
+
+type LineNumberedError interface {
+	Error() string
+	TraceInfo() string
+}
+
+func RaiseFrom(base error, current error) *ReRaisableError {
+	var message string
+	if lineNumberedError, ok := current.(LineNumberedError); ok {
+		message = lineNumberedError.Error() + lineNumberedError.TraceInfo()
+	} else {
+		message = current.Error() + GetTraceInfo()
+	}
+	return &ReRaisableError{
+		base:         base,
+		message:      message,
+		currentError: current,
+	}
+}
+
+func GetTraceInfo() string {
+	pc, fileName, fileLine, ok := runtime.Caller(2)
+	details := runtime.FuncForPC(pc)
+	if ok && details != nil {
+		return fmt.Sprintf("func %s() at %s:%d", details.Name(), fileName, fileLine)
+	}
+	return ""
+}
