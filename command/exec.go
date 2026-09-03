a88ff2b23acebf273ef142b41343848f2340ea38
kisun <kisun668@gmail.com>
2026-09-03 13:37:01 +0800

    feat: 新增直接指定程序与参数列表的命令执行方式

--- command/exec.go
+++ command/exec.go
@@ -22,10 +22,6 @@ func ExecuteWithContext(ctx context.Context, cmdline string, options ...CmdOptio
 		err = errors.Wrapf(err, "execute `%s`", cmdline)
 	}()
 
-	if ctx == nil {
-		return 1, "", errors.New("nil context")
-	}
-
 	opt := defaultCmdOptions
 	for _, option := range options {
 		option(&opt)
@@ -34,9 +30,63 @@ func ExecuteWithContext(ctx context.Context, cmdline string, options ...CmdOptio
 		return 1, "", err
 	}
 
-	argList := make([]string, 0)
+	argList := make([]string, 0, len(opt.callerArgs)+1)
 	argList = append(argList, opt.callerArgs...)
 	argList = append(argList, cmdline)
+
+	return runProgram(ctx, cmdline, opt, opt.caller, argList)
+}
+
+func Execute(cmdline string, options ...CmdOption) (exit int, output string, err error) {
+	return ExecuteWithContext(context.Background(), cmdline, options...)
+}
+
+// ExecuteArgsWithContext 直接执行指定程序与参数列表，不经过 shell
+// （caller）包装。与 ExecuteWithContext（把命令行字符串交给
+// cmd.exe /c 执行）不同，本函数将参数原样传递给目标程序，
+// 路径中含空格或特殊字符时不会引入额外的引号问题。
+//
+// 注意：选项中的 caller / callerArgs 设置在本函数中不生效，
+// 仅 WithDebug / WithTimeout / WithDir / WithEnv 有效。
+func ExecuteArgsWithContext(ctx context.Context, program string, args []string, options ...CmdOption) (exit int, output string, err error) {
+	display := program + " " + strings.Join(args, " ")
+	defer func() {
+		err = errors.Wrapf(err, "execute `%s`", display)
+	}()
+
+	if program == "" {
+		return 1, "", errors.New("please specify program")
+	}
+
+	opt := defaultCmdOptions
+	for _, option := range options {
+		option(&opt)
+	}
+
+	return runProgram(ctx, display, opt, program, args)
+}
+
+func ExecuteArgs(program string, args []string, options ...CmdOption) (exit int, output string, err error) {
+	return ExecuteArgsWithContext(context.Background(), program, args, options...)
+}
+
+// runProgram 是 ExecuteWithContext 与 ExecuteArgsWithContext 的
+// 共享执行核心：解析程序路径、应用超时、采集输出并计算退出码。
+func runProgram(ctx context.Context, display string, opt cmdConfig, program string, argList []string) (exit int, output string, err error) {
+	if ctx == nil {
+		return 1, "", errors.New("nil context")
+	}
+
+	if opt.dir != "" {
+		if _, e := os.Stat(opt.dir); e != nil {
+			return 1, "", e
+		}
+	}
+
+	programPath, err := exec.LookPath(program)
+	if err != nil {
+		return 1, "", err
+	}
 
 	cmdCtx := ctx
 	if opt.timeout > 0 {
@@ -45,19 +95,14 @@ func ExecuteWithContext(ctx context.Context, cmdline string, options ...CmdOptio
 		defer cancel()
 	}
 
-	callerPath, err := exec.LookPath(opt.caller)
-	if err != nil {
-		return 1, "", err
-	}
-
 	if opt.debug {
 		defer func() {
-			logger.Debugf("ExecuteWithContext: exec `%s`\nreturn:%v\noutput:\n%s\nerror:%v",
-				cmdline, exit, output, err)
+			logger.Debugf("runProgram: exec `%s`\nreturn:%v\noutput:\n%s\nerror:%v",
+				display, exit, output, err)
 		}()
 	}
 
-	cmdProc := exec.CommandContext(cmdCtx, callerPath, argList...)
+	cmdProc := exec.CommandContext(cmdCtx, programPath, argList...)
 	cmdProc.Dir = opt.dir
 	cmdProc.Env = opt.env
 
@@ -94,10 +139,6 @@ func ExecuteWithContext(ctx context.Context, cmdline string, options ...CmdOptio
 	}
 
 	return
-}
-
-func Execute(cmdline string, options ...CmdOption) (exit int, output string, err error) {
-	return ExecuteWithContext(context.Background(), cmdline, options...)
 }
 
 func checkOptions(opt cmdConfig) error {
