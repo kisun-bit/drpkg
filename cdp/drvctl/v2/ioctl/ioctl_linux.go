88f74eaa6ea27480981a01fd93f537d31f5cccf2
kisun <kisun668@gmail.com>
2026-09-14 09:51:54 +0800

    feat: 结构调整

--- cdp/drvctl/v2/ioctl/ioctl_linux.go
+++ cdp/drvctl/v2/ioctl/ioctl_linux.go
@@ -9,6 +9,14 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+const (
+	// ringbuffer未正常工作 在设置一致性标记时，但共享缓存已经满了就会返回该错误码
+	ERROR_RINGBUFFER_NOT_WORKING_FORBID_CLEAR_BIT = 140
+
+	// 内存分配失败 任何接口都有可能返回该错误码
+	ERR_NO_MEMORY = 102
+)
+
 // doIoctl 是平台无关的 IOCTL 内部入口。
 //
 // 在 Linux 平台上，ioctl 系统调用复用输入缓冲区存储应答数据，
